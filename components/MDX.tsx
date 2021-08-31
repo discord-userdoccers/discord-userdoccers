8023ee0b08d8d8a221bd0b589ce9a126399d138b
Leah Lundqvist <leah@pigeon.sh>
2021-08-31 22:33:58 +0200

    type MDX.tsx

--- components/MDX.tsx
+++ components/MDX.tsx
@@ -3,22 +3,26 @@ import ContentWrapper from "./mdx/ContentWrapper";
 import Heading from "./mdx/Heading";
 import Code from "./mdx/Code";
 
-function InlineCode(props) {
+const InlineCode: React.FC<{}> = (props) => {
   return <code {...props} />;
 }
 
+type HeadingProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>;
+
 const COMPONENTS = {
-  wrapper: (props) => <ContentWrapper {...props} />,
-  h1: (props) => <Heading as="h1" {...props} />,
-  h2: (props) => <Heading as="h2" {...props} />,
-  h3: (props) => <Heading as="h3" {...props} />,
-  h4: (props) => <Heading as="h4" {...props} />,
-  h5: (props) => <Heading as="h5" {...props} />,
-  h6: (props) => <Heading as="h6" {...props} />,
+  wrapper: (props: any) => <ContentWrapper {...props} />,
+  h1: (props: HeadingProps) => <Heading as="h1" {...props} />,
+  h2: (props: HeadingProps) => <Heading as="h2" {...props} />,
+  h3: (props: HeadingProps) => <Heading as="h3" {...props} />,
+  h4: (props: HeadingProps) => <Heading as="h4" {...props} />,
+  h5: (props: HeadingProps) => <Heading as="h5" {...props} />,
+  h6: (props: HeadingProps) => <Heading as="h6" {...props} />,
   code: Code,
   inlineCode: InlineCode,
 };
 
-export default function MDX({ children }) {
+const MDX: React.FC<{}> = ({ children }) => {
   return <MDXProvider components={COMPONENTS}>{children}</MDXProvider>;
 }
+
+export default MDX;
