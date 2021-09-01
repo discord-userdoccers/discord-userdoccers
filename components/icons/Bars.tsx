6a51ffbb84b2dcb2efb5ec7b9c05af4702517776
Ian Mitchell <ian.mitchell@hey.com>
2021-09-01 14:25:14 -0700

    Style Updates

--- components/icons/Bars.tsx
+++ components/icons/Bars.tsx
@@ -1,7 +1,12 @@
 export default function Bars(props: any) {
   return (
-    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" className="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" {...props}>
-      <path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 448 512"
+      {...props}
+    >
+      <path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path>
     </svg>
   );
 }
--- components/mdx/ContentWrapper.tsx
+++ components/mdx/ContentWrapper.tsx
@@ -9,20 +9,23 @@ interface ContentWrapperProps {
 }
 
 export default function ContentWrapper({ children }: ContentWrapperProps) {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
-  const fadeClasses = classNames("absolute top-0 left-0 h-full w-full bg-black z-30 duration-300 md:pointer-events-none md:opacity-0", {
-    "opacity-50": sidebarOpen,
-    "opacity-0 pointer-events-none": !sidebarOpen
-  })
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const fadeClasses = classNames(
+    "absolute z-30 left-0 top-0 w-full h-full bg-black duration-300 md:opacity-0 md:pointer-events-none",
+    {
+      "opacity-50": sidebarOpen,
+      "opacity-0 pointer-events-none": !sidebarOpen,
+    }
+  );
   return (
     <Fragment>
-      <div className="flex h-screen bg-white overflow-hidden">
-        <div className={fadeClasses} onClick={() => setSidebarOpen(false)}/>
-        <Menu open={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
+      <div className="dark:bg-background-dark flex h-screen bg-white overflow-hidden">
+        <div className={fadeClasses} onClick={() => setSidebarOpen(false)} />
+        <Menu open={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
         <main className="relative flex flex-1 flex-col items-center focus:outline-none overflow-y-auto">
           <Header setSidebarOpen={setSidebarOpen} />
-          <div className="pb-10 pt-10 px-10 xl:w-2/3 w-full">
+          <div className="pb-10 pt-10 px-10 w-full max-w-6xl">
             <article className="m-auto">{children}</article>
           </div>
         </main>
