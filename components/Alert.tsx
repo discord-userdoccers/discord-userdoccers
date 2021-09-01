d020244f40b526cdb8cb65c0c72526ec12f45392
Soriquato <65538890+Soriquato@users.noreply.github.com>
2021-09-01 20:57:20 +0200

    Update Alert.tsx

--- components/Alert.tsx
+++ components/Alert.tsx
@@ -4,9 +4,9 @@ type AlertType = "danger" | "warn" | "info";
 
 function getClasses(type: AlertType) {
   return classNames("alert block p-4 rounded-lg", {
-    "bg-red-100 text-red-500": type === "danger",
-    "bg-yellow-100 text-yellow-500": type === "warn",
-    "bg-blue-100 text-blue-500": type === "info",
+    "bg-red-100 text-red-500 border-2 border-red-500 m-0": type === "danger",
+    "bg-yellow-100 text-yellow-500 border-2 border-yellow-500 m-0": type === "warn",
+    "bg-blue-100 text-blue-500 border-2 border-blue-500 m-0": type === "info",
   });
 }
 
