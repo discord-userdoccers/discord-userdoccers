9904d057436790a05efa2366b66455f0d43ea77c
ckohen <chaikohen@gmail.com>
2021-08-31 20:47:36 -0700

    Snowflake Deconstruction but Vectorized

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import "tailwindcss/tailwind.css";
 import "../stylesheets/alert.css";
 import "../stylesheets/whitney/whitney.css";
 import "../stylesheets/code.css";
+import "../stylesheets/snowflake-deconstruction.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
