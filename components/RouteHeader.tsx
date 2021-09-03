5f9215f04867107ef97cf4e83146449f66d2f81a
Vlad Frangu <kingdgrizzle@gmail.com>
2021-09-04 01:31:14 +0300

    chore: small changes

--- components/RouteHeader.tsx
+++ components/RouteHeader.tsx
@@ -28,7 +28,7 @@ function MethodBadge({ method }: MethodBadgeProps) {
 function AuditLogHeaderBadge() {
   return (
     <Link href="/resources/audit-log#x-audit-log-reason">
-      <a className="inline-flex items-center px-2.5 py-0.5 dark:text-text-dark text-text-light font-mono text-xs font-medium dark:bg-theme-dark-sidebar bg-theme-light-sidebar rounded-full">
+      <a className="inline-flex items-center px-2.5 py-0.5 dark:text-text-dark text-text-light font-mono text-xs font-medium dark:bg-theme-dark-sidebar bg-theme-light-sidebar rounded-full focus:outline-none focus-visible:ring-2 focus-visible:ring-brand-blurple focus-visible:ring-opacity-75">
         <abbr
           title="Supports X-Audit-Log-Reason Header"
           className="no-underline"
