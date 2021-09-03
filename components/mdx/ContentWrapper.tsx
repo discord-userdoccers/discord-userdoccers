c0b89f01094fc13f5f0f92c32fbeaf7f3ba16d10
Ian Mitchell <ian.mitchell@hey.com>
2021-09-03 15:34:33 -0700

    spacing fixes

--- components/mdx/ContentWrapper.tsx
+++ components/mdx/ContentWrapper.tsx
@@ -8,7 +8,7 @@ export default function ContentWrapper({ children }: ContentWrapperProps) {
   return (
     <div className="scroll-pt-16 md:scroll-pt-0 relative flex flex-1 flex-col items-center focus:outline-none overflow-y-auto">
       <Header />
-      <main className="pt-4 px-4 w-full max-w-6xl sm:pt-0 sm:px-6 lg:px-10">
+      <main className="p-4 w-full max-w-6xl sm:pb-6 sm:pt-0 sm:px-6 lg:pb-10 lg:px-10">
         <article className="m-auto mt-0 md:mt-4">{children}</article>
       </main>
     </div>
