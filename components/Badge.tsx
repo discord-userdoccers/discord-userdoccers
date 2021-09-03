2227eac76b132e5b385e842975d4c432b74e4536
Vlad Frangu <kingdgrizzle@gmail.com>
2021-09-04 02:17:54 +0300

    fix: requested changes 2

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
 
-export interface BadgeProps {
+interface BadgeProps {
   href: string;
   tooltip: string;
   name: string;
 }
 
-export default function Badge(props: BadgeProps) {
+export default function Badge({ href, name, tooltip }: BadgeProps) {
   return (
-    <Link href={props.href}>
+    <Link href={href}>
       <a className="inline-flex items-center px-2.5 py-0.5 dark:text-text-dark text-text-light font-mono text-xs font-medium dark:bg-theme-dark-sidebar bg-theme-light-sidebar rounded-full focus:outline-none focus-visible:ring-2 focus-visible:ring-brand-blurple focus-visible:ring-opacity-75">
-        <abbr title={props.tooltip} className="no-underline">
-          {props.name}
+        <abbr title={tooltip} className="no-underline">
+          {name}
         </abbr>
       </a>
     </Link>
