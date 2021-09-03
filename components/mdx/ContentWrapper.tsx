e6f883ff5afa3074253736f40408b338cc6b2c1f
Ian Mitchell <ian.mitchell@hey.com>
2021-09-03 14:55:28 -0700

    aesthetics

--- components/mdx/ContentWrapper.tsx
+++ components/mdx/ContentWrapper.tsx
@@ -9,7 +9,7 @@ export default function ContentWrapper({ children }: ContentWrapperProps) {
     <div className="scroll-pt-16 md:scroll-pt-0 relative flex flex-1 flex-col items-center focus:outline-none overflow-y-auto">
       <Header />
       <main className="pt-4 px-4 w-full max-w-6xl sm:pt-0 sm:px-6 lg:px-10">
-        <article className="m-auto">{children}</article>
+        <article className="m-auto mt-0 md:mt-10">{children}</article>
       </main>
     </div>
   );
