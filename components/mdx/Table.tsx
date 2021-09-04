5df46170e5da3a08a258b589758c66387ebcfb2d
Leah Lundqvist <leah@pigeon.sh>
2021-09-04 14:40:41 -0700

    improve look of tables on light theme

--- components/mdx/Table.tsx
+++ components/mdx/Table.tsx
@@ -12,7 +12,7 @@ export function Table(props: JSX.IntrinsicElements["table"]) {
 export function TableHead(props: JSX.IntrinsicElements["thead"]) {
   return (
     <thead
-      className="dark:bord-white text-left border-b-2 dark:border-black border-gray-400"
+      className="dark:bord-white text-left border-b-2 dark:border-black border-gray-300"
       {...props}
     />
   );
@@ -21,7 +21,7 @@ export function TableHead(props: JSX.IntrinsicElements["thead"]) {
 export function TableHeader(props: JSX.IntrinsicElements["th"]) {
   return (
     <th
-      className="p-2 bg-indigo-100 dark:bg-table-head-background-dark uppercase"
+      className="p-2 px-3 bg-gray-200 dark:bg-table-head-background-dark uppercase"
       {...props}
     />
   );
@@ -30,12 +30,12 @@ export function TableHeader(props: JSX.IntrinsicElements["th"]) {
 export function TableRow(props: JSX.IntrinsicElements["tr"]) {
   return (
     <tr
-      className="dark:even:bg-trueGray-900 dark:bg-trueGray-800 dark:text-text-dark text-text-light even:bg-gray-100 bg-gray-200"
+      className="dark:even:bg-trueGray-900 dark:bg-trueGray-800 dark:text-text-dark text-text-light even:bg-gray-50 bg-gray-100"
       {...props}
     />
   );
 }
 
 export function TableData(props: JSX.IntrinsicElements["td"]) {
-  return <td className="p-2 max-w-xs" {...props} />;
+  return <td className="p-2 px-3 max-w-xs" {...props} />;
 }
