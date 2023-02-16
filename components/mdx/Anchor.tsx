4d5b59dedea05a351062f620f45a86bc9976d933
Splatterxl <splatterxl@outlook.ie>
2023-02-16 22:42:02 +0000

    try to fix type errors
    Signed-off-by: Splatterxl <splatterxl@outlook.ie>

--- components/mdx/Anchor.tsx
+++ components/mdx/Anchor.tsx
@@ -1,22 +1,23 @@
 import classNames from "classnames";
-import Link from "next/link";
+import Link, { LinkProps } from "next/link";
 
-type DocLinkProps = JSX.IntrinsicElements["a"] & {
-  href: string;
-};
+// hack to make the props like each other
+type DocLinkProps = React.ForwardRefExoticComponent<
+  React.AnchorHTMLAttributes<HTMLAnchorElement> &
+    LinkProps & {
+      children?: React.ReactNode | null;
+    } & React.RefAttributes<HTMLAnchorElement>
+> & { href: string };
 
 function DocLink({ href, ...props }: DocLinkProps) {
-  return (
-    (<Link href={href} {...props}>
-
-    </Link>)
-  );
+  return <Link href={href} {...props} />;
 }
 
 export default function Anchor({ href, className, ...props }: JSX.IntrinsicElements["a"]) {
   const classes = classNames("text-brand-link hover:underline", className);
 
   if (href?.startsWith("/")) {
+    // @ts-expect-error This works but TypeScript hates it. Can't be bothered finding out why it errors
     return <DocLink href={href} className={classes} {...props} />;
   }
 
