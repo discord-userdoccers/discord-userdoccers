f41ac24360877aba377a77a243844247b2f378fc
dolfies <jeyalfie47@gmail.com>
2023-02-17 19:24:07 -0500

    Add flags to more channel examples

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -194,7 +194,9 @@ export default function Navigation() {
                 Thread Metadata Object
               </NavigationSubLink>
               <NavigationSubLink href="/resources/channel#thread-member-object">Thread Member Object</NavigationSubLink>
-              <NavigationSubLink href="/resources/channel#default-reaction-object">Default Reaction Object</NavigationSubLink>
+              <NavigationSubLink href="/resources/channel#default-reaction-object">
+                Default Reaction Object
+              </NavigationSubLink>
               <NavigationSubLink href="/resources/channel#forum-tag-object">Forum Tag Object</NavigationSubLink>
               <NavigationSubLink href="/resources/channel#endpoints">Endpoints</NavigationSubLink>
             </Fragment>
