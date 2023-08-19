5848a67a8f6b54970f087aa3e2fafe81615d855f
dolfies <jeyalfie47@gmail.com>
2023-08-19 18:44:40 +0300

    Revert "Document invite metadata consolidation changes"
    This reverts commit 708422df4dd9223a2361048fbea4472a997c7740.

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -365,6 +365,9 @@ export default function Navigation() {
             <Fragment>
               <NavigationSubLink href="/resources/invite#temporary-invites">Temporary Invites</NavigationSubLink>
               <NavigationSubLink href="/resources/invite#invite-object">Invite Object</NavigationSubLink>
+              <NavigationSubLink href="/resources/invite#invite-metadata-object">
+                Invite Metadata Object
+              </NavigationSubLink>
               <NavigationSubLink href="/resources/invite#invite-guild-object">Invite Guild Object</NavigationSubLink>
               <NavigationSubLink href="/resources/invite#invite-channel-object">
                 Invite Channel Object
