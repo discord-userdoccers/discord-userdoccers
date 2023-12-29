6e39e72e98ce7916521e07e02a963e6215689834
Amia <9750071+aamiaa@users.noreply.github.com>
2023-12-29 04:16:22 +0100

    Revert "yes yes prettier fix"
    This reverts commit 327b6ccd0d299438a38964143d2012bd8e80e4c6.

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -241,7 +241,9 @@ export default function Navigation() {
                 Thread Metadata Object
               </NavigationSubLink>
               <NavigationSubLink href="/resources/channel#thread-member-object">Thread Member Object</NavigationSubLink>
-              <NavigationSubLink href="/resources/channel#partial-emoji-object">Partial Emoji Object</NavigationSubLink>
+              <NavigationSubLink href="/resources/channel#partial-emoji-object">
+                Partial Emoji Object
+              </NavigationSubLink>
               <NavigationSubLink href="/resources/channel#forum-tag-object">Forum Tag Object</NavigationSubLink>
               <NavigationSubLink href="/resources/channel#endpoints">Endpoints</NavigationSubLink>
             </Fragment>
