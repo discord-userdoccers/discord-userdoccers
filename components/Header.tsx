995d1372a910f22604b2062346dd6dd9daf6e011
dolfies <jeyalfie47@gmail.com>
2024-01-27 18:57:06 -0500

    Remove (seemingly) useless menu page?

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
 
   return (
     <header className="sticky z-10 left-0 top-0 flex items-center justify-between p-2 w-full h-16 dark:bg-theme-dark-sidebar bg-theme-light-sidebar md:hidden md:p-4">
-      <Link href="/menu" onClick={onMenuClick}>
+      <Link href="/" onClick={onMenuClick}>
         <Bars
           onClick={setOpen}
           className="justify-self-start ml-1 h-7 text-black dark:text-white cursor-pointer md:hidden"
