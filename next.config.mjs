94ab89899694f7c3d80b83e0805ce31006cd60e9
splatter <splatterxl@outlook.ie>
2024-08-07 15:34:27 +0100

    add base domain to env
    Signed-off-by: splatter <splatterxl@outlook.ie>

--- next.config.mjs
+++ next.config.mjs
@@ -7,13 +7,22 @@ const config = {
   basePath: "",
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   swcMinify: true,
+  env: {
+    BASE_DOMAIN:
+      process.env.NODE_ENV === "production"
+        ? process.env.VERCEL_PROJECT_PRODUCTION_URL ?? 'docs.discord.sex'
+        : process.env.VERCEL_URL ?? "localhost:3000",
+  },
+  // eslint-disable-next-line @typescript-eslint/require-await -- required for Next.js
   async redirects() {
-    return [{
-      source:'/',
-      destination:'/intro',
-      permanent: true,
-    }]
-  }
+    return [
+      {
+        source: "/",
+        destination: "/intro",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 const withMDX = createMDX({
