105a1eeda2ff18abdc1006af6182869cbd8ebd55
voidfill <71205200+voidfill@users.noreply.github.com>
2024-08-11 14:12:46 +0200

    fixes, optimizations

--- src/nano.ts
+++ src/nano.ts
@@ -1,4 +1,20 @@
-import { atom, batched } from "nanostores";
+import { atom, batched, type ReadableAtom } from "nanostores";
+
+export function createSelector<T>(source: ReadableAtom<T>): (key: T, listener: (value: boolean) => void) => void {
+	const m = new Map<T, ((v: boolean) => void)[]>();
+	source.listen((newValue, oldValue) => {
+		if (oldValue === newValue) return;
+		for (const l of m.get(oldValue) ?? []) l(false);
+		for (const l of m.get(newValue) ?? []) l(true);
+	});
+
+
+	return (key, listener) => {
+		if (!m.has(key)) m.set(key, []);
+		m.get(key)!.push(listener);
+		listener(source.value === key);
+	};
+}
 
 export const browserTheme = atom<"light" | "dark">("dark");
 export const userTheme = atom<"light" | "dark" | "system">("system");
