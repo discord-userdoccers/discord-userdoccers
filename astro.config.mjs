5a5e68db34a5f1af5d70cfb6de25044341157b06
voidfill <71205200+voidfill@users.noreply.github.com>
2024-08-15 21:40:48 +0200

    fixes, SEO stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import remarkSuperSub from "remark-supersub";
-import node from "@astrojs/node";
 import mdx from "@astrojs/mdx";
 import { shikiDiffNotation } from "./shiki/diffnotation";
 import { shikiMetaParser } from "./shiki/metaparser";
@@ -11,11 +10,8 @@ import { shikiLineNumbers } from "./shiki/linenumbers";
 // https://astro.build/config
 export default defineConfig({
 	output: "static",
-	site: "https://docs.discord.sex",
+	site: "https://userdoccers.voidfill.cc",
 	integrations: [tailwind(), sitemap(), mdx()],
-	adapter: node({
-		mode: "standalone",
-	}),
 	redirects: {
 		"/": "intro",
 	},
