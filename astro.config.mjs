a7403f7f8e91784c737774dcff7a410604e0b3ed
voidfill <71205200+voidfill@users.noreply.github.com>
2024-08-29 23:13:24 +0200

    Add search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,5 @@
+import { fileURLToPath } from "node:url";
+import { dirname, relative } from "node:path";
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
@@ -6,12 +8,34 @@ import mdx from "@astrojs/mdx";
 import { shikiDiffNotation } from "./shiki/diffnotation";
 import { shikiMetaParser } from "./shiki/metaparser";
 import { shikiLineNumbers } from "./shiki/linenumbers";
+import { spawn } from "node:child_process";
 
 // https://astro.build/config
 export default defineConfig({
 	output: "static",
 	site: "https://userdoccers.voidfill.cc",
-	integrations: [tailwind(), sitemap(), mdx()],
+	integrations: [
+		tailwind(),
+		sitemap(),
+		mdx(),
+		{
+			name: "pagefind-post-build",
+			hooks: {
+				"astro:build:done": ({ dir }) => {
+					const targetDir = fileURLToPath(dir);
+					const cwd = dirname(fileURLToPath(import.meta.url));
+					const relativeDir = relative(cwd, targetDir);
+					return new Promise((resolve) => {
+						spawn("pnpm", ["dlx", "pagefind", "--site", relativeDir], {
+							stdio: "inherit",
+							shell: true,
+							cwd,
+						}).on("close", resolve);
+					});
+				},
+			},
+		},
+	],
 	redirects: {
 		"/": "intro",
 	},
