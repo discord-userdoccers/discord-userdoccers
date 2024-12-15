9dc8d7396dd836cfee67bdec1cb7a8ee21a450a8
Haruka <personal@shiroko.me>
2024-12-15 03:55:58 +0200

    feat: account revert (#298)
    Co-authored-by: dolfies <jeyalfie47@gmail.com>

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -164,6 +164,7 @@ export default function Navigation() {
               <NavigationSubLink href="/authentication#login">Login</NavigationSubLink>
               <NavigationSubLink href="/authentication#register">Register</NavigationSubLink>
               <NavigationSubLink href="/authentication#logout">Logout</NavigationSubLink>
+              <NavigationSubLink href="/authentication#password-recovery">Password Recovery</NavigationSubLink>
               <NavigationSubLink href="/authentication#account-recovery">Account Recovery</NavigationSubLink>
               <NavigationSubLink href="/authentication#mfa-verification">MFA Verification</NavigationSubLink>
             </Fragment>
