9948f25dcec429fd2098243a5b5a3eabe23594fb
splatter <splatterxl@outlook.ie>
2025-02-16 20:24:23 +0000

    add types for eslint
    Signed-off-by: splatter <splatterxl@outlook.ie>

--- components/navigation/NavigationList.tsx
+++ components/navigation/NavigationList.tsx
@@ -1,10 +1,29 @@
 import { NavigationSection, NavigationLink, NavigationSubLink, SearchItem } from "./NavigationItems";
 import data from "./data.json" with { type: "json" };
 
+export interface NavigationData {
+  name: string | null;
+  pages: Page[];
+  section: string;
+}
+
+export interface Page {
+  name: string;
+  link: string;
+  subLinks: SubLink[];
+  sort: number;
+}
+
+export interface SubLink {
+  link: string;
+  name: string;
+  level: number;
+}
+
 export default function NavigationList() {
   return (
     <>
-      {data.map((section) => (
+      {(data as any as NavigationData[]).map((section) => (
         <NavigationSection title={section.name ?? undefined} key={section.section}>
           {section.name === null ? SearchItem : null}
           {Object.values(section.pages).map((page) => (
