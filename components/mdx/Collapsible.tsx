e35508cb7ab336f9b85306e767293db5823a9643
splatter <splatterxl@outlook.ie>
2025-02-17 23:50:45 +0000

    implement icon & animation
    Signed-off-by: splatter <splatterxl@outlook.ie>

--- components/mdx/Collapsible.tsx
+++ components/mdx/Collapsible.tsx
@@ -1,27 +1,59 @@
-import { PropsWithChildren, useState } from "react";
+import { createElement, PropsWithChildren, ReactNode, useMemo, useState } from "react";
 import Paragraph from "./Paragraph";
+import MDX_ICONS from "./icons";
 import Chevron from "../icons/Chevron";
 
 interface CollapsibleProps {
-  // TODO: use icon
-  icon?: React.ReactNode;
+  icon?: Exclude<React.ReactNode, string> | keyof typeof MDX_ICONS;
   title: string;
   description: string;
 }
 
 export default function Collapsible({
   children: originalChildren,
-  icon,
+  icon: originalIcon,
   title,
   description,
 }: PropsWithChildren<CollapsibleProps>) {
   const [isOpen, setIsOpen] = useState(false);
 
-  // for some reason the first element in children is a string but the rest are <p> elements, this is just a hack
-  // FIXME(splatterxl): why is this needed?
+  // sometimes the first element in children is a string but the rest are <p> elements
+  //
+  // only if the element is called like this:
+  // <Collapsible ...>
+  //   some text <-- LITERAL STRING
+  //
+  //   some other text <-- <P> ELEMENT
+  // </Collapsible>
+  //
+  // it's probably incorrect usage in MDX standard but we handle it here anyway
   const children = Array.isArray(originalChildren) ? Array.from(originalChildren) : [originalChildren];
   if (typeof children[0] === "string") {
     children[0] = <Paragraph>{children[0]}</Paragraph>;
+  }
+
+  // convert `warning-icon` and `warning` to `WarningIcon`
+  const possibleKey = useMemo(() => {
+    if (typeof originalIcon !== "string") return;
+
+    // uses regex to uppercase the first character and any characters with - before it
+    const replaced = originalIcon
+      .toLowerCase()
+      .replace(
+        /^(\w)|-(\w)/g,
+        (_, a: string | undefined, b: string | undefined) => a?.toUpperCase() ?? b!.toUpperCase(),
+      );
+
+    // allow users to specify `-icon` if they want
+    if (replaced.endsWith("Icon")) return replaced;
+    return `${replaced}Icon`;
+  }, [originalIcon]) as keyof typeof MDX_ICONS | undefined;
+
+  // prepare the React node
+  let icon: ReactNode = null;
+  if (typeof originalIcon === "string") {
+    if (originalIcon in MDX_ICONS) icon = createElement(MDX_ICONS[originalIcon as keyof typeof MDX_ICONS]);
+    else if (possibleKey && possibleKey in MDX_ICONS) icon = createElement(MDX_ICONS[possibleKey]);
   }
 
   return (
@@ -35,15 +67,14 @@ export default function Collapsible({
       >
         <div className="flex flex-col gap-1">
           <h3 className="flex items-center justify-start gap-1.5 text-xl">
-            <div className="collapsible-icon">{icon}</div>
+            {icon && <div className="collapsible-icon">{icon}</div>}
             {title}
           </h3>
           <p className="text-base leading-6 text-text-light dark:text-text-dark">{description}</p>
         </div>
         <Chevron
-          className="mr-2 size-4 opacity-50 transition-all group-hover:opacity-100"
+          className="mr-2 size-5 opacity-70 transition-all group-hover:opacity-100"
           style={{
-            // rotate 180 deg if open
             transform: isOpen ? "rotate(-180deg)" : "none",
           }}
         />
