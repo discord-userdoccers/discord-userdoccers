cdf47389b13b053fcccd7977eb2966f370ce53c3
megumin <megumin@megu.dev>
2025-04-28 20:19:22 +0100

    feat: code generation for tables (#349)

--- components/mdx/Table.tsx
+++ components/mdx/Table.tsx
@@ -1,10 +1,163 @@
-export function Table(props: JSX.IntrinsicElements["table"]) {
+import React, { RefObject, useEffect, useRef, useState } from "react";
+import { PythonGenerator } from "../../lib/type-generator/python";
+import { useCodegenLanguage, useToast } from "../../lib/type-generator/store";
+import { TypescriptGenerator } from "../../lib/type-generator/typescript";
+
+import Chevron from "../icons/Chevron";
+import CopyIcon from "../icons/Copy";
+import Python from "../icons/Python";
+import TickIcon from "../icons/Tick";
+import TypeScript from "../icons/TypeScript";
+
+const cn = (...c: string[]) => c.join(" ");
+
+function CopyBar(props: { tableRef: RefObject<HTMLTableElement> }) {
+  const { showSuccessToast, showErrorToast } = useToast();
+  const { selectedLanguage, setSelectedLanguage } = useCodegenLanguage();
+
+  const [isDropdownOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const [showCopyIcon, setShowCopyIcon] = useState(false);
+
+  useEffect(() => {
+    if (showCopyIcon) {
+      setTimeout(() => {
+        setShowCopyIcon(false);
+      }, 1000);
+    }
+  }, [showCopyIcon]);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
+
+  function tryCopyCodeToClipboard() {
+    if (!props.tableRef.current) return;
+
+    let code;
+    if (selectedLanguage === "Python") {
+      code = new PythonGenerator(props.tableRef.current).generateCode();
+    }
+    if (selectedLanguage === "TypeScript") {
+      code = new TypescriptGenerator(props.tableRef.current).generateCode();
+    }
+
+    if (code) {
+      (async () => {
+        await navigator.clipboard
+          .writeText(code)
+          .catch((err) => {
+            console.error(err);
+            showErrorToast("Failed to copy code to clipboard. Check console for details.");
+          })
+          .then(() => {
+            showSuccessToast("Copied code to clipboard.");
+            setShowCopyIcon(true);
+          });
+      })();
+    } else {
+      showErrorToast("Failed to generate code for this table. Check console for details.");
+      console.error(
+        "Failed to generate code for this table.",
+        "It could be that this table isn't showing an actual struct or enum.",
+        "If this is supposed to work, open an issue on GitHub!",
+      );
+    }
+  }
+
   return (
-    <div className="max-w-full overflow-auto">
+    <div className="opacity-transition relative flex flex-row-reverse items-center gap-1 opacity-0 duration-300 group-hover:opacity-100">
+      {showCopyIcon && (
+        <TickIcon className="my-auto h-5 w-5 cursor-pointer select-none" onClick={() => tryCopyCodeToClipboard()} />
+      )}
+      {!showCopyIcon && (
+        <CopyIcon className="my-auto h-5 w-5 cursor-pointer select-none" onClick={() => tryCopyCodeToClipboard()} />
+      )}
+
+      <div className="relative" ref={dropdownRef}>
+        <button
+          type="button"
+          className={cn(
+            "flex cursor-pointer select-none gap-1 rounded-md bg-white py-1 pl-2 pr-1.5 font-mono text-xs text-text-light dark:bg-black dark:text-text-dark",
+            "flex items-center gap-1",
+          )}
+          onClick={() => setIsOpen(!isDropdownOpen)}
+          title="Select language to copy"
+        >
+          {selectedLanguage === "Python" && <Python className="h-4" />}
+          {selectedLanguage === "TypeScript" && <TypeScript className="h-4" />}
+
+          <span>{selectedLanguage}</span>
+
+          <Chevron className="h-4 w-3 text-black dark:text-white" />
+        </button>
+        {isDropdownOpen && (
+          <div className="absolute right-0 z-10 mt-1 w-32 rounded-md shadow-lg">
+            <div role="menu" aria-orientation="vertical" aria-labelledby="options-menu-button">
+              <button
+                onClick={() => {
+                  setSelectedLanguage("Python");
+                  setIsOpen(false);
+                }}
+                className={cn(
+                  "block w-full cursor-pointer select-none rounded-t-md px-4 py-2 text-left font-mono text-xs",
+                  "flex items-center gap-2",
+                  "bg-white dark:bg-theme-dark-sidebar",
+                  "hover:bg-theme-light-sidebar-hover dark:hover:bg-theme-dark-sidebar-hover",
+                )}
+                role="menuitem"
+              >
+                <Python className="h-5" />
+                Python
+              </button>
+              <button
+                onClick={() => {
+                  setSelectedLanguage("TypeScript");
+                  setIsOpen(false);
+                }}
+                className={cn(
+                  "block w-full cursor-pointer select-none rounded-b-md px-4 py-2 text-left font-mono text-xs",
+                  "flex items-center gap-2",
+                  "bg-white dark:bg-theme-dark-sidebar",
+                  "hover:bg-theme-light-sidebar-hover dark:hover:bg-theme-dark-sidebar-hover",
+                )}
+                role="menuitem"
+              >
+                <TypeScript className="h-5" />
+                TypeScript
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export function Table(props: JSX.IntrinsicElements["table"]) {
+  const tableRef = useRef<HTMLTableElement>(null);
+
+  return (
+    <div className="group relative mt-0 max-w-full overflow-auto">
       <table
-        className="w-full border-collapse overflow-hidden break-words rounded-md align-middle text-sm"
+        ref={tableRef}
+        className="w-full border-collapse overflow-hidden break-words rounded-md rounded-t-none align-middle text-sm"
         {...props}
       />
+      <div className="absolute right-0 top-0 p-2 px-2">
+        <CopyBar tableRef={tableRef} />
+      </div>
     </div>
   );
 }
---
+++ lib/type-generator/typescript.ts
@@ -0,0 +1,164 @@
+import { TableType, Tokenizer, TypeInfo } from "./tokenizer";
+
+const TYPE_MAP: [string | RegExp, string][] = [
+  ["snowflake", "Snowflake"],
+  ["ISO8601 timestamp", "string"],
+  ["file contents", "UInt8Array"],
+  [/^binary data/i, "Uint8Array"],
+  // Handles most numeric types.
+  [/^(signed|unsigned)?\s?(byte|short|integer)/i, "number"],
+];
+
+export class TypescriptGenerator {
+  public readonly tokenizer: Tokenizer;
+
+  public constructor(rootElement: HTMLTableElement) {
+    this.tokenizer = new Tokenizer(rootElement);
+  }
+
+  public generateCode() {
+    const layout = this.tokenizer.getLayout();
+    if (!layout) throw new Error("Invalid layout received.");
+
+    const title = this.typeToString(layout.title);
+    const description = layout.description?.multiline ?? [];
+
+    const properties = [];
+
+    for (const property of layout.contents) {
+      const isEnum = layout.type === TableType.Enum;
+      const field = this.typeToString(property.field, !isEnum);
+
+      const isDeprecated = this.typeToString(property.field).includes("(deprecated)");
+
+      let type = this.typeToString(property.type, !isEnum);
+      if (!isEnum) type = this.typeMapper(type);
+
+      const description = property.description ? this.typeToString(property.description) : "";
+
+      const otherColumns = [];
+      for (const column of property.otherColumns) {
+        otherColumns.push([this.typeToString(column[0]), this.typeToString(column[1])]);
+      }
+
+      properties.push({
+        field,
+        type,
+        description,
+        otherColumns,
+        isDeprecated,
+      });
+    }
+
+    let output = "";
+
+    if (description.length) {
+      output += "/**\n";
+      description.forEach((line, i) => {
+        output += ` * ${line}\n`;
+        if (i < description.length - 1) output += ` *\n`;
+      });
+      output += " */\n";
+    }
+
+    if (layout.type === TableType.Struct) {
+      output += `export interface ${title} {\n`;
+    } else if (layout.type === TableType.Enum || layout.type === TableType.Event) {
+      output += `export enum ${title} {\n`;
+    } else {
+      // TODO: Should I Object.freeze()?
+      output += `const ${title} = {\n`;
+    }
+
+    for (const property of properties) {
+      if (property.description || property.otherColumns.length) {
+        output += `\t/**\n`;
+
+        if (property.description) output += `\t * ${property.description}\n`;
+        if (property.description && property.otherColumns.length) {
+          output += `\t *\n`; // add an extra newline for spacing.
+        }
+        for (const [key, value] of property.otherColumns) {
+          output += `\t * ${key}: ${value}\n`;
+        }
+        if (property.isDeprecated) {
+          output += `\t * @deprecated\n`;
+        }
+        output += `\t */\n`;
+      }
+
+      if (layout.type === TableType.Struct) {
+        output += `\t${property.field}: ${property.type};\n`;
+      } else if (layout.type === TableType.Enum || layout.type === TableType.Event) {
+        output += `\t${property.field} = ${property.type},\n`;
+      } else {
+        const [left, right] = property.type.split("<<").map((s) => s.trim());
+        output += `\t${property.field}: ${left}n << ${right}n,\n`;
+      }
+    }
+
+    if (layout.type === TableType.Struct || layout.type === TableType.Enum || layout.type === TableType.Event) {
+      output += `}\n`;
+    } else {
+      output += `} as const;\n`;
+    }
+
+    output += "\n";
+
+    return output;
+  }
+
+  private typeMapper(input: string): string {
+    for (const [k, v] of TYPE_MAP) {
+      if (k instanceof RegExp) {
+        if (input.match(k)) {
+          return v;
+        }
+      }
+      if (input === k) {
+        return v;
+      }
+    }
+    return input;
+  }
+
+  private typeToString(type: TypeInfo, onlyFirstWord = false, isInArray = false): string {
+    if (type.array) {
+      if (type.array.length === 1) {
+        // arrays in userdoccers are defined as array[T]
+        const inner = this.typeToString(type.array[0], onlyFirstWord, true);
+        return `${inner}[]`;
+      } else if (type.array.length > 1) {
+        // tuples in userdoccers are defined as array[T1, T2, ...]
+        const inner = type.array
+          .map((i) => this.typeToString(i, onlyFirstWord, true))
+          .map((i) => this.typeMapper(i))
+          .join(", ");
+        return `[${inner}]`;
+      }
+    }
+    if (type.map) {
+      const left = this.typeToString(type.map[0]);
+      const right = this.typeToString(type.map[1]);
+      return `Record<${this.typeMapper(left)}, ${this.typeMapper(right)}>`;
+    }
+    if (type.multiline) {
+      return type.multiline.join("\n");
+    }
+    if (type.type && type.optional) {
+      const t = this.typeMapper(type.type);
+      if (isInArray) {
+        return `(${t} | null)`;
+      }
+      return `${t} | null`;
+    }
+    if (type.type && onlyFirstWord) {
+      return type.type.split(" ")[0];
+    }
+    if (typeof type.type === "string") {
+      return type.type;
+    }
+
+    throw new Error("Invalid TypeInfo provided.");
+  }
+}
