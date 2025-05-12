f38493d21b1a65d248378263049ba4c6238ad78e
Han Seung Min - 한승민 <hanseungmin@proton.me>
2025-05-13 02:56:02 +0530

    docsearch polyfill (#360)
    Co-authored-by: splatter <splatterxl@outlook.ie>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @next/next/no-css-tags, @next/next/no-sync-scripts */
 import { Head, Html, Main, NextScript } from "next/document";
 
+const ALGOLIA_HOST = `https://${process.env.NEXT_PUBLIC_ALGOLIA_APP_ID ?? "JAJUDFJBI4"}-dsn.algolia.net`;
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -9,11 +11,7 @@ export default function Document() {
           <script src={process.env.NEXT_PUBLIC_REACT_DEVTOOLS_ADDRESS} />
         ) : null}
 
-        <link
-          rel="preconnect"
-          href={`https://${process.env.NEXT_PUBLIC_ALGOLIA_APP_ID ?? "JAJUDFJBI4"}-dsn.algolia.net`}
-          crossOrigin="anonymous"
-        />
+        <link rel="preconnect" href={ALGOLIA_HOST} crossOrigin="anonymous" />
       </Head>
       <body>
         <Main />
