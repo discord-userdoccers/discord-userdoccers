d8c6878e3608ac3ea49f0921b26e9870a6098efa
MCausc78 <mcausc78@gmail.com>
2025-05-14 01:50:53 +0300

    Inline renderedPages

--- components/navigation/NavigationList.tsx
+++ components/navigation/NavigationList.tsx
@@ -26,28 +26,27 @@ export default function NavigationList() {
       {(data as any as NavigationData[]).map((section) => {
         const pages = Array.isArray(section.pages) ? section.pages : Object.values(section.pages);
         pages.sort((a, b) => (a.sort === b.sort ? a.name.localeCompare(b.name) : a.sort + b.sort));
-        const renderedPages = pages.map((page) => (
-          <NavigationLink
-            href={page.link}
-            subLinks={
-              page.subLinks.length === 0
-                ? null
-                : page.subLinks.map((subLink) => (
-                    <NavigationSubLink href={subLink.link} key={subLink.link}>
-                      {subLink.name}
-                    </NavigationSubLink>
-                  ))
-            }
-            key={page.link}
-          >
-            {page.name}
-          </NavigationLink>
-        ));
 
         return (
           <NavigationSection title={section.name ?? undefined} key={section.section}>
             {section.name === null ? SearchItem : null}
-            {renderedPages}
+            {pages.map((page) => (
+              <NavigationLink
+                href={page.link}
+                subLinks={
+                  page.subLinks.length === 0
+                    ? null
+                    : page.subLinks.map((subLink) => (
+                        <NavigationSubLink href={subLink.link} key={subLink.link}>
+                          {subLink.name}
+                        </NavigationSubLink>
+                      ))
+                }
+                key={page.link}
+              >
+                {page.name}
+              </NavigationLink>
+            ))}
           </NavigationSection>
         );
       })}
