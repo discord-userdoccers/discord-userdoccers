f01ee75c603c148d85be0bdc426d648980b540eb
MCausc78 <mcausc78@gmail.com>
2025-05-14 00:56:23 +0300

    Prefer page.sort over page.name if not equal

--- components/navigation/NavigationList.tsx
+++ components/navigation/NavigationList.tsx
@@ -3,7 +3,7 @@ import data from "./data.json" with { type: "json" };
 
 export interface NavigationData {
   name: string | null;
-  pages: Page[];
+  pages: Page[] | Record<string, Page>;
   section: string;
 }
 
@@ -23,30 +23,34 @@ export interface SubLink {
 export default function NavigationList() {
   return (
     <>
-      {(data as any as NavigationData[]).map((section) => (
-        <NavigationSection title={section.name ?? undefined} key={section.section}>
-          {section.name === null ? SearchItem : null}
-          {Object.values(section.pages)
-            .toSorted((a, b) => a.name.localeCompare(b.name))
-            .map((page) => (
-              <NavigationLink
-                href={page.link}
-                subLinks={
-                  page.subLinks.length === 0
-                    ? null
-                    : page.subLinks.map((subLink) => (
-                        <NavigationSubLink href={subLink.link} key={subLink.link}>
-                          {subLink.name}
-                        </NavigationSubLink>
-                      ))
-                }
-                key={page.link}
-              >
-                {page.name}
-              </NavigationLink>
-            ))}
-        </NavigationSection>
-      ))}
+      {(data as any as NavigationData[]).map((section) => {
+        const pages = Array.isArray(section.pages) ? section.pages : Object.values(section.pages);
+        pages.sort((a, b) => (a.sort === b.sort ? a.name.localeCompare(b.name) : a.sort + b.sort));
+        const renderedPages = pages.map((page) => (
+          <NavigationLink
+            href={page.link}
+            subLinks={
+              page.subLinks.length === 0
+                ? null
+                : page.subLinks.map((subLink) => (
+                    <NavigationSubLink href={subLink.link} key={subLink.link}>
+                      {subLink.name}
+                    </NavigationSubLink>
+                  ))
+            }
+            key={page.link}
+          >
+            {page.name}
+          </NavigationLink>
+        ));
+
+        return (
+          <NavigationSection title={section.name ?? undefined} key={section.section}>
+            {section.name === null ? SearchItem : null}
+            {renderedPages}
+          </NavigationSection>
+        );
+      })}
     </>
   );
 }
