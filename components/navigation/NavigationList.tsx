5f71eb50e190f4e32b4182d1c1ad596abad26eb3
MCausc78 <mcausc78@gmail.com>
2025-05-18 02:42:23 +0300

    I'm clueless on this one, but I'll try anyway

--- components/navigation/NavigationList.tsx
+++ components/navigation/NavigationList.tsx
@@ -3,7 +3,7 @@ import data from "./data.json" with { type: "json" };
 
 export interface NavigationData {
   name: string | null;
-  pages: Page[] | Record<string, Page>;
+  pages: Page[];
   section: string;
 }
 
@@ -23,33 +23,28 @@ export interface SubLink {
 export default function NavigationList() {
   return (
     <>
-      {(data as any as NavigationData[]).map((section) => {
-        const pages = Array.isArray(section.pages) ? section.pages : Object.values(section.pages);
-        pages.sort((a, b) => (a.sort === b.sort ? a.name.localeCompare(b.name) : a.sort + b.sort));
-
-        return (
-          <NavigationSection title={section.name ?? undefined} key={section.section}>
-            {section.name === null ? SearchItem : null}
-            {pages.map((page) => (
-              <NavigationLink
-                href={page.link}
-                subLinks={
-                  page.subLinks.length === 0
-                    ? null
-                    : page.subLinks.map((subLink) => (
-                        <NavigationSubLink href={subLink.link} key={subLink.link}>
-                          {subLink.name}
-                        </NavigationSubLink>
-                      ))
-                }
-                key={page.link}
-              >
-                {page.name}
-              </NavigationLink>
-            ))}
-          </NavigationSection>
-        );
-      })}
+      {(data as any as NavigationData[]).map((section) => (
+        <NavigationSection title={section.name ?? undefined} key={section.section}>
+          {section.name === null ? SearchItem : null}
+          {Object.values(section.pages).map((page) => (
+            <NavigationLink
+              href={page.link}
+              subLinks={
+                page.subLinks.length === 0
+                  ? null
+                  : page.subLinks.map((subLink) => (
+                      <NavigationSubLink href={subLink.link} key={subLink.link}>
+                        {subLink.name}
+                      </NavigationSubLink>
+                    ))
+              }
+              key={page.link}
+            >
+              {page.name}
+            </NavigationLink>
+          ))}
+        </NavigationSection>
+      ))}
     </>
   );
 }
