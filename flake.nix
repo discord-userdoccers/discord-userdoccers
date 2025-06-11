21aa70d31440d547ee6a9515c4194f99b2c37605
한승민 <hanseungmin@proton.me>
2025-06-11 17:26:11 +0900

    fix(flake.nix): patch workerd to work on nixos

--- flake.nix
+++ flake.nix
@@ -22,7 +22,6 @@
       default = with pkgs;
         mkShell {
           buildInputs = [
-            prettier
             typescript-language-server
 
             nodejs
@@ -30,6 +29,18 @@
 
             nil
           ];
+
+          shellHook = ''
+            if [[ $(uname -a) == *"NixOS"* ]] then
+                if [[ ! -d node_modules/.pnpm ]] then
+                    echo "You are on NixOS, please reload the dev shell after \`pnpm install\`"
+                else
+                    echo "Patching workerd to work on NixOS"
+
+                    ${pkgs.fd}/bin/fd -H -I -t executable workerd node_modules/.pnpm -x ${pkgs.patchelf}/bin/patchelf --set-interpreter ${pkgs.glibc}/lib/ld-linux-x86-64.so.2
+                fi
+            fi
+          '';
         };
     });
   };
