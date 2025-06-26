b04d1453008e025823cd4e18052a2566422069a0
splatter <splatterxl@outlook.ie>
2025-06-26 16:41:14 +0100

    fix checkbox layout on mobile
    Signed-off-by: splatter <splatterxl@outlook.ie>

--- pages/datamining/errors.tsx
+++ pages/datamining/errors.tsx
@@ -68,31 +68,33 @@ export default function Errors() {
 
       <div className="flex justify-start">
         <button
-          className="inline-flex items-center gap-2 rounded-md px-5 py-2.5 text-md/80 bg-brand-blurple font-semibold text-white focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white hover:bg-brand-blurple/90 data-open:bg-gray-700"
+          className="inline-flex items-center gap-2 rounded-md px-4 md:px-5 py-2 md:py-2.5 text-lg/70 md:text-md/80 bg-brand-blurple font-semibold text-white focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white hover:bg-brand-blurple/90 data-open:bg-gray-700"
           onClick={() => {}}
         >
           Submit Error
         </button>
 
-        {/* this searchbar is very laggy, any fix? -- maybe debounce */}
-        <input
-          type="text"
-          placeholder="Search error codes..."
-          // FIXME(splatter): This is disgusting styling
-          className="DocSearch DocSearch-Button py-3.5 px-4 h-full min-w-96 rounded-md dark:bg-table-row-background-secondary-dark focus:outline-none focus:ring-2 focus:ring-brand-blurple"
-          onChange={(e) => setSearch(e.target.value.toLowerCase())}
-        />
-
-        <div className="flex self-end items-center gap-2 px-4">
+        <div className="flex flex-col lg:flex-row flex-start gap-1 lg:gap-0">
+          {/* this searchbar is very laggy, any fix? -- maybe debounce */}
           <input
-            type="checkbox"
-            id="filter-by-unknown"
-            checked={filterByUnknown}
-            onChange={(e) => setFilterByUnknown(e.target.checked)}
-            className="h-4 w-4 rounded border-gray-300 text-brand-blurple focus:ring-brand-blurple"
+            type="text"
+            placeholder="Search error codes..."
+            // FIXME(splatter): This is disgusting styling
+            className="DocSearch DocSearch-Button py-3.5 px-4 h-full w-full sm:min-w-64 md:min-w-96 rounded-md dark:bg-table-row-background-secondary-dark focus:outline-none focus:ring-2 focus:ring-brand-blurple"
+            onChange={(e) => setSearch(e.target.value.toLowerCase())}
           />
 
-          <label htmlFor="filter-by-unknown">Filter by Unknown</label>
+          <div className="flex w-full md:self-end items-center gap-2 px-4">
+            <input
+              type="checkbox"
+              id="filter-by-unknown"
+              checked={filterByUnknown}
+              onChange={(e) => setFilterByUnknown(e.target.checked)}
+              className="h-4 w-4 rounded border-gray-300 text-brand-blurple focus:ring-brand-blurple"
+            />
+
+            <label htmlFor="filter-by-unknown">Filter by Unknown</label>
+          </div>
         </div>
       </div>
 
