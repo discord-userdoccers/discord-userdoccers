b47593fcf39d7e8f649fcfff745de2a621cce40d
splatter <splatterxl@outlook.ie>
2025-06-29 01:09:19 +0100

    fix codegen (#388)

--- components/mdx/Table.tsx
+++ components/mdx/Table.tsx
@@ -126,7 +126,8 @@ function CopyBar(props: { tableRef: RefObject<HTMLTableElement> }) {
   );
 }
 
-export function Table(props: React.JSX.IntrinsicElements["table"]) {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars -- needed to avoid react bug
+export function Table({ ref: _, ...props }: React.JSX.IntrinsicElements["table"]) {
   const tableRef = useRef<HTMLTableElement>(null);
 
   return (
